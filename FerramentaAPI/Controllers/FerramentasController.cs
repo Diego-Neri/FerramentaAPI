bb9842735951e454ac50e383cfb914191a01babb
Mayara Pereira <mayaracapereira45@gmail.com>
2025-04-07 20:47:08 -0300

    Teste de controller

---
+++ FerramentaAPI/Controllers/FerramentasController.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FerramentaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Ferramentas")]
+    public class FerramentasController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Testando");
+        
+        
+    }
+}
