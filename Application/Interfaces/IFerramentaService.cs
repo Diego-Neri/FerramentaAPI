b74b83a3891211b2937becb3b9ba54f6b06d1d7b
Diego-Neri <diegoneri500@gmail.com>
2025-04-08 20:28:06 -0300

    Interfaces

---
+++ Application/Interfaces/IFerramentaService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces {
+    public interface IFerramentaService {
+
+        FerramentaDTO GetFerramentaById(int id);
+        IEnumerable<FerramentaDTO> GetAllFerramentas();
+        void AddFerramenta(FerramentaCreateDTO ferramentaDTO);
+        void UpdateFerramenta(int id, FerramentaCreateDTO ferramentaDTO);
+        void DeleteFerramenta(int id);
+    }
+}
