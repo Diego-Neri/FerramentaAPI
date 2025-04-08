80e87c0971bc38f6b9361fd49eb9d7c540ec57b6
Diego-Neri <diegoneri500@gmail.com>
2025-04-08 20:28:51 -0300

    Enums

---
+++ FerramentaAPI.Domain/Enums/TipoFerramenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Domain.Enums {
+    public enum TipoFerramenta {
+        VBit,
+        TopoRaso
+    }
+}
