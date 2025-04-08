60931714aeff48316dfb92c516efab653e7cd009
Diego-Neri <diegoneri500@gmail.com>
2025-04-08 20:29:46 -0300

    InterfacesDomain

---
+++ FerramentaAPI.Domain/Interfaces/IFerramentaRepository.cs
@@ -0,0 +1,16 @@
+﻿using FerramentaAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Domain.Interfaces {
+    public interface IFerramentaRepository {
+        void Add(IFerramenta ferramenta);
+        IFerramenta GetById(int id);
+        IEnumerable<IFerramenta> GetAll();
+        void Update(IFerramenta ferramenta);
+        void Delete(int id);
+    }
+}
