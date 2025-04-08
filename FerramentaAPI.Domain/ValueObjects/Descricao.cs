c783488616af679fe6210f19c847c1e8a504eaf3
Diego-Neri <diegoneri500@gmail.com>
2025-04-08 19:56:16 -0300

    ValueObjects

---
+++ FerramentaAPI.Domain/ValueObjects/Descricao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Domain.ValueObjects {
+    public class Descricao {
+        public string Valor { get; private set; }
+
+        public Descricao(string valor) {
+            if (string.IsNullOrWhiteSpace(valor) || valor.Length < 3)
+                throw new ArgumentException("Descrição deve ter no mínimo 3 caracteres.");
+            Valor = valor;
+        }
+    }
+}
---
+++ FerramentaAPI.Domain/ValueObjects/EnderecoFerramenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Domain.ValueObjects {
+    public class EnderecoFerramenta {
+        public string Valor { get; private set; }
+
+        public EnderecoFerramenta(string valor) {
+
+            if (string.IsNullOrWhiteSpace(valor) || valor.Length < 1 || valor.Length > 50)
+                throw new ArgumentException("Endereço deve ter entre 1 e 50 caracteres.");
+            Valor = valor;
+        }
+    }
+}
