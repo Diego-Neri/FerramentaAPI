24b92a88173206b8593086f03d6c6120ff7be104
Diego-Neri <diegoneri500@gmail.com>
2025-04-08 20:27:49 -0300

    DTOs

---
+++ Application/DTOs/FerramentaCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using FerramentaAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs {
+    public class FerramentaCreateDTO {
+        public string Endereco { get; set; }
+        public string Descricao { get; set; }
+        public double Diametro { get; set; }
+        public double Altura { get; set; }
+        public TipoFerramenta Tipo { get; set; }
+
+    }
+}
---
+++ Application/DTOs/FerramentaDTO.cs
@@ -0,0 +1,19 @@
+﻿using FerramentaAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs {
+    public class FerramentaDTO {
+
+        public int Id { get; set; }
+        public string Endereco { get; set; }
+        public string Descricao { get; set; }
+        public double Diametro { get; set; }
+        public double Altura { get; set; }
+        public TipoFerramenta Tipo { get; set; }
+        public string Path { get; set; }
+    }
+}
