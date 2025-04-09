2425a5f0aee1651d1a04e95398befd4f25526c3a
Diego-Neri <diegoneri500@gmail.com>
2025-04-09 17:45:56 -0300

    Testes Unitários

---
+++ FerramentaAPI.Domain.Test/FerramentaTest.cs
@@ -0,0 +1,23 @@
+﻿using FerramentaAPI.Domain.Entities;
+using FerramentaAPI.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Domain.Test {
+    public class FerramentaTest {
+
+        [Fact]
+        public void VBit_Path_RetornarSubirNosCantos() {
+
+            //Arrange
+            var endereco = new EnderecoFerramenta("Rua A, 123");
+            var descricao = new Descricao("Ferramenta de teste");
+            var vbit = new VBit(endereco, descricao, 10, 10);
+            Assert.Equal("Subir nos cantos", vbit.Path());
+        }
+
+    }
+}
