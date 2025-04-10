38ec8b7017e629fe64392c848c68ae5fa3ecc7a6
Diego-Neri <diegoneri500@gmail.com>
2025-04-10 16:45:39 -0300

    Add dependências Moq

---
+++ FerramentaAPI.Application.Test/FerramentaServicesTest.cs
@@ -0,0 +1,18 @@
+﻿using FerramentaAPI.Infra.Test;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerramentaAPI.Application.Test {
+    public class FerramentaServicesTest : InMemoryFerramentaRepositoryTest {
+        [Fact]
+
+
+        
+
+
+    }
+}
